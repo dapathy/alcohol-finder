3121158791d53d212ecad808855b13a99aee9b3f
Tyler Doster <tyler.doster@dh.com>
2024-09-01 18:18:45 -0700

    use sessions

--- alcoholFinder.py
+++ alcoholFinder.py
@@ -2,16 +2,21 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import googlemaps
+import logging
 
 from productLocation import ProductLocation
 
 googleMapsApiKey = os.environ['GOOGLE_MAPS_API_KEY']
+session = requests.Session()
+session.headers.update({'Host': 'www.oregonliquorsearch.com'})
 
 def establishSession():
-    url = "http://www.oregonliquorsearch.com"
-    requestHeaders = {'Host': 'www.oregonliquorsearch.com'}
-    response = requests.get(url, headers=requestHeaders, allow_redirects=True)
-    return response.cookies["JSESSIONID"]
+    response = session.get("http://www.oregonliquorsearch.com")
+    logging.info(f"Session established with SessionId {response.cookies['JSESSIONID']}")
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Origin': 'http://www.oregonliquorsearch.com', 'Referer': 'http://www.oregonliquorsearch.com/home.jsp'}
+    data = {'btnSubmit': "I'm 21 or older"}
+    response = session.post("http://www.oregonliquorsearch.com/servlet/WelcomeController", allow_redirects=True, headers=headers, data=data)
+    logging.info(f"Status code after age verification: {response.status_code}")
 
 def getDistanceMatrix(origin, destinations):
     mapsApi = googlemaps.Client(key=googleMapsApiKey)
@@ -29,8 +34,9 @@ def getDistanceMatrix(origin, destinations):
 
 def queryProduct(sessionId, itemCode):
     url = f"http://www.oregonliquorsearch.com/product_details.jsp?productRowNum=1&rowCount=45&column=City&pageCurrent=1&pageCount=1&itemDisplay={itemCode}"
-    cookies = {'JSESSIONID': sessionId}
-    response = requests.get(url, cookies=cookies)
+    response = session.get(url)
+    logging.info(f"Query response status code: {response.status_code}")
+
     soup = BeautifulSoup(response.text, 'html.parser')
     locations = []
     for row in soup.find_all(class_='alt-row'):
