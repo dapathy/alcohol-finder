6e8549a46b1ac965830b75b902a45ece0e273135
Tyler Doster <tyler.doster@dh.com>
2024-09-09 17:16:16 -0700

    adjust schedule

--- function_app.py
+++ function_app.py
@@ -10,7 +10,7 @@ app = func.FunctionApp()
 run_on_startup = 'pydevd' in sys.modules # Check if the debugger is attached
 
 @app.function_name(name="findAlcoholScheduled")
-@app.timer_trigger(schedule="0 30 9 * * *", arg_name="myTimer", run_on_startup=run_on_startup, use_monitor=False) 
+@app.timer_trigger(schedule="0 30 16 * * *", arg_name="myTimer", run_on_startup=run_on_startup, use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     itemCode = os.environ['ITEM_CODE']
     origin = os.environ['ORIGIN_ADDRESS']
